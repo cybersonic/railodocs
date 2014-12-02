a2d616d8bd414f95a6049ef7d6f15dc07042f719
unknown <mark.drew@HBPC0106.webres.local>
2014-12-02 17:38:35 +0000

    Adding tag formatting.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var expressLayouts = require('express-ejs-layouts');
 var http = require('http').Server(app);
 var ejs = require('ejs');
+var fs = require('fs');
 
 var version = require('./controllers/version');
 var tag = require('./controllers/tag');
 var func = require('./controllers/function');
-
 
 
 app.set('view engine', 'ejs');
@@ -15,22 +15,10 @@ app.set('layout', "layout");
 app.use(expressLayouts);
 app.use(express.static(__dirname + '/public'));
 
-
-console.log("env: ", app.settings.env);
-
-app.get('/api/version', version.current);
-app.get('/api/versions', version.list);
-app.get('/api/current', version.current);
-app.get('/api/tags', tag.list);
-
-app.get('/api/tag/:id', tag.get);
-app.get('/api/functions', func.list);
-app.get('/api/function/:id', func.get);
-
-app.get('/versions', version.list);
-app.get('/tags', tag.list);
+app.get('/versions*', version.list);
+app.get('/tags*', tag.list);
 app.get('/tag/:id', tag.get);
-app.get('/functions', func.list);
+app.get('/functions*', func.list);
 app.get('/function/:id', func.get);
 
 
