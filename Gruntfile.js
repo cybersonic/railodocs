9de7e54942c3b71e5f98f2a950b4e2eb40e7c0da
unknown <Mark.Drew@HBPC0106.webres.local>
2014-12-10 12:26:36 +0000

    Adding and removing styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,35 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    watch: {
-      js: {
-        files: [
-          'app.js',
-          'routes/**/*.js',
-          'lib/*.js'
-        ],
-        tasks: ['develop'],
-        options: { nospawn: true }
+    clean : ["dist/"],
+    concat: {
+       
+       js: { src: ['public/js/*.js'], dest: 'public/dist/railodocs.js' },
+       css: { src: ['public/css/*.css'], dest: 'public/dist/railodocs.css' }
+      }, 
+    uglify: {
+      options: {},
+      dist: { files: { "public/dist/railodocs.min.js": ['public/dist/railodocs.js'] }}
+      },
+    cssmin: {
+      main: {
+        files: [{
+                  expand: true,
+                  cwd: 'public/css/',
+                  src: ['*.css', '!*.min.css'],
+                  dest: 'public/dist/',
+                  ext: '.min.css'
+                }]
+          }
       }
-    },
-    develop: {
-      server: {
-        file: 'app.js'
-      }
-    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-develop');
 
-  grunt.registerTask('default', ['develop']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'cssmin']);
 
 };
